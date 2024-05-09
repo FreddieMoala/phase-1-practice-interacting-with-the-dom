b9de35230bf9cb557eb8853f1a4fc535bd3dd66a
Siaosi Moala <loto_toa@hotmail.com>
2024-05-09 13:54:17 +1000

    DONE

---
+++ js/challenge.js
@@ -0,0 +1,58 @@
+function timer() {
+  return setInterval(function () {
+    const counter = document.getElementById('counter');
+    const count = parseInt(counter.innerText);
+    counter.innerText = count + 1;
+  }, 1000);
+}
+
+let isDisabled = false;
+let interval = timer(); 
+const pauseBtn = document.getElementById('pause');
+pauseBtn.addEventListener('click', () => {
+  if (isDisabled) {
+    isDisabled = false;
+    interval = timer();
+    pauseBtn.innerText = 'pause';
+  } else {
+    isDisabled = true;
+    clearInterval(interval);
+    pauseBtn.innerText = 'resume';
+  }
+  const buttons = document.getElementsByTagName('button');
+  for (let button of buttons) {
+    if (button.id !== 'pause') {
+      button.disabled = isDisabled;
+    }
+  }
+});
+
+const minusBtn = document.querySelector('#minus');
+const plusBtn = document.querySelector('#plus');
+const heartBtn = document.querySelector('#heart');
+
+const counter = document.querySelector('#counter');
+
+minusBtn.addEventListener('click', () => {
+  counter.innerText = parseInt(counter.innerText) - 1;
+});
+
+plusBtn.addEventListener('click', () => {
+    counter.innerText = parseInt(counter.innerText) + 1;
+    });
+
+
+let likeCounts = {};
+
+heartBtn.addEventListener('click', () => {
+  const counterValue = counter.innerText;
+  if (!likeCounts[counterValue]) {
+    likeCounts[counterValue] = 0;
+  }
+  likeCounts[counterValue]++;
+  
+  const likes = document.querySelector('.likes');
+  const like = document.createElement('li');
+  like.innerText = `${counterValue} has been liked ${likeCounts[counterValue]} times`;
+  likes.appendChild(like);
+});
